c1cce365632366a2c3ebba6c683c46f2ecf4940d
Rishu kumar singh <rishu272018@gmail.com>
2026-02-09 21:46:43 +0530

    input and typecasting

---
+++ Input&Typecasting.py
@@ -0,0 +1,38 @@
+# Input and Typecasting
+
+# name=input("Enter your name:")
+# print("Welcome ", name)
+
+# age=int(input("Enter your age:"))
+# print(type(age))
+# age=age+5
+# print("Your age is :", age)
+
+# temprature=float(input("Enter today's temp :"))
+# print(type(temprature))
+
+#Convert number to string
+# sales = 50000
+# text = "Total sales: " + str(sales)
+# print(text)
+
+
+
+#Total Sales Calculator
+product = input("Product Name: ")
+quantity = int(input("Enter quantity sold: "))
+price_per_unit = float(input("Enter price per unit"))
+
+total_sales = quantity * price_per_unit
+
+
+print("______________________")
+print("Product:", product)
+print("Total Sales Amount =", total_sales)
+
+
+fnum=int(input("Enter first num:"))
+snum=int(input("Enter Second num:"))
+
+sum=fnum+snum
+print(sum)
