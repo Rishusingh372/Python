7db119779bc20b9105ce9b9cacf7317fa9ea4474
Rishu kumar singh <rishu272018@gmail.com>
2026-02-10 23:16:11 +0530

    string Indexing

---
+++ string_Indexing.py
@@ -0,0 +1,38 @@
+# String Indexing
+
+# name="satish"
+# print(name)
+# print(name[0])
+# print(name[-5])
+
+# # String Slicing
+
+# product="Laptop pro 2024"
+
+# print(product[-4:])
+
+
+
+
+
+text = "DataAnalysis"
+
+# # Extracting first 4 characters
+# print("First 4 letters:", text[0:4])    # Data
+
+
+# # Extracting characters from middle
+# print("Middle slice:", text[4:12])      # Analysis
+
+# # Extract till end
+# print("Till end:", text[4:])            # Analysis
+
+# # Extract from beginning
+# print("From start:", text[:4])          # Data
+
+# # Extract last 5 characters
+# print("Last 5 letters:", text[-5:])     # alysis
+
+#Skip Text "DataAnalysis"
+print("Skip Text :", text[0:12:3])
+print("Reverse :", text[::-1])
